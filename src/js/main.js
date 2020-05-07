f4792b4c305072960b65420490f775f974d894b8
Michal Frelik <michal.frelik1@gmail.com>
2020-05-07 14:14:14 +0200

    refactoring

--- src/js/main.js
+++ src/js/main.js
@@ -20,3 +20,51 @@ if ('serviceWorker' in navigator) {
 console.log(`Hello world!`);
 
 
+const saveButton = document.querySelector('.button__save--js');
+console.log(saveButton);
+
+const loadButton = document.querySelector('.button__load--js');
+console.log(loadButton);
+
+const textarea = document.querySelector('.textarea--js');
+console.log(textarea);
+
+saveButton.addEventListener('click', (e) => {
+  localStorage.getItem('textarea', e.target.value);
+})
+
+
+// const inputTest = document.querySelector('.form__window-js');
+
+// inputTest.addEventListener('keyup', (e) => {
+//   localStorage.setItem('inputTest',e.target.value);
+// });
+
+// if (localStorage.getItem('inputTest')) {
+//   inputTest.value = (localStorage.getItem('inputTest'))
+// };
+
+
+
+// const navigationSwitcher = document.querySelector('.navigation__switcher--js');
+
+// navigationSwitcher.addEventListener('click', (e) => {
+// 	const navigationList = document.querySelector('.navigation__list--js');
+// 	navigationList.classList.toggle('navigation__list--visible');
+// 	navigationSwitcher.innerHTML = 'X';
+// });
+
+
+// const button = document.querySelector('.header__button--js');
+// console.log(button);
+
+// button.addEventListener('click', (e) => {
+// 	const header = document.querySelector('.header__title--js')
+// 	header.innerHTML = '!! Pierwszy button !!';
+// 	header.classList.toggle('header__title--red');
+// 	if (header.classList.contains('header__title--red')) {
+// 		console.log('jest klasa');
+// 	} else {
+// 		console.log('brak klasy');
+// 	}
+// });
