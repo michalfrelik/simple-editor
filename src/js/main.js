b8e22c0f045ffcb53a7166458d0deb0617bda318
Michal Frelik <michal.frelik1@gmail.com>
2020-05-14 14:00:07 +0200

    feat: 3 levels of saving + memory cleaning

--- src/js/main.js
+++ src/js/main.js
@@ -18,15 +18,46 @@ if ('serviceWorker' in navigator) {
 
 
 const textarea = document.querySelector('.form__textarea--js');
-const load = document.querySelector('.form__button--load-js');
-const save = document.querySelector('.form__button--save-js');
+const load1 = document.querySelector('.form__button--load-js1');
+const save1 = document.querySelector('.form__button--save-js1');
+const load2 = document.querySelector('.form__button--load-js2');
+const save2 = document.querySelector('.form__button--save-js2');
+const load3 = document.querySelector('.form__button--load-js3');
+const save3 = document.querySelector('.form__button--save-js3');
+const clear = document.querySelector('.form__button--clear-js');
 
-save.addEventListener('click', (e) => {
+save1.addEventListener('click', (e) => {
   e.preventDefault();
-  localStorage.setItem('textarea', textarea.value);
+  localStorage.setItem('textarea1', textarea.value);
 });
 
-load.addEventListener('click', (e) => {
+load1.addEventListener('click', (e) => {
   e.preventDefault();
-  textarea.value = localStorage.getItem('textarea');
+  textarea.value = localStorage.getItem('textarea1');
 });
+
+save2.addEventListener('click', (e) => {
+  e.preventDefault();
+  localStorage.setItem('textarea2', textarea.value);
+});
+
+load2.addEventListener('click', (e) => {
+  e.preventDefault();
+  textarea.value = localStorage.getItem('textarea2');
+});
+
+save3.addEventListener('click', (e) => {
+  e.preventDefault();
+  localStorage.setItem('textarea3', textarea.value);
+});
+
+load3.addEventListener('click', (e) => {
+  e.preventDefault();
+  textarea.value = localStorage.getItem('textarea3');
+});
+
+clear.addEventListener('click', (e) => {
+  localStorage.removeItem('textarea1');
+  localStorage.removeItem('textarea2');
+  localStorage.removeItem('textarea3');
+})
